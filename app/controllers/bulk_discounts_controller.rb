d6cc7714789599ad35ad13b992c5645cc8a03bd9
Sam Devine <samuel.will.devine@gmail.com>
2021-11-15 15:00:40 -0700

    feat: delete discount button

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -21,6 +21,14 @@ class BulkDiscountsController < ApplicationController
     end
   end
 
+  def destroy
+    @merchant = Merchant.find(params[:merchant_id])
+    bulk_discount = BulkDiscount.find(params[:id])
+
+    bulk_discount.delete
+    redirect_to merchant_bulk_discounts_path(@merchant)
+  end
+
   private
   def bulk_discount_params
     params.require(:bulk_discount).permit(:name, :percentage, :quantity)
