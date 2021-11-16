eaecb17b0e430fc5555a289e7fd12f2b38242ca9
Sam Devine <samuel.will.devine@gmail.com>
2021-11-16 15:58:10 -0700

    feature complete

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -4,10 +4,6 @@ class InvoiceItem < ApplicationRecord
 
   enum status: [ :pending, :packaged, :shipped ]
 
-  def self.total_revenue(invoice)
-    where(invoice_id: invoice.id).sum("invoice_items.unit_price * invoice_items.quantity")
-  end
-
   def revenue
     quantity * unit_price / 100.0
   end
